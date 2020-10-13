975bfbb5a1f3fd07c9fde9df69233aa8b391d7c8
BondareVR <slbondarev22@gmail.com>
2020-10-13 22:32:21 +0300

    First commit

---
+++ Models/ApartmentTipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveBRD.Models
+{
+    public class ApartmentTipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
